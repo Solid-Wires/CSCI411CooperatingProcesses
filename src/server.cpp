1e3039d0e43f7e92c6ea5fc15b542b0b1e566996
Solid-Wires <solid.wires@gmail.com>
2024-02-12 00:49:52 -0500

    commit

--- src/server.cpp
+++ src/server.cpp
@@ -8,7 +8,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+<<<<<<< HEAD
 #include <unistd.h>
+=======
+>>>>>>> 531a37c (commit)
 
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -85,7 +88,10 @@ int main ()
         // The following copies an integer (token) into the c-string that is the output buffer
 		sprintf(out_buffer, "%d", token_number);
 
+<<<<<<< HEAD
         usleep(3 * 1000000);
+=======
+>>>>>>> 531a37c (commit)
         // Send the message to the client's mailbox/mailqueue
 		if (mq_send (qd_client, out_buffer, strlen (out_buffer), 0) == -1) {
             cerr << "Server: Not able to send message to client";
