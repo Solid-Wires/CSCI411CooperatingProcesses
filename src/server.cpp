fad22272786a515c6e22fcf02e3617bb648471cd
Solid-Wires <solid.wires@gmail.com>
2024-02-12 09:59:46 -0500

    assert

--- src/server.cpp
+++ src/server.cpp
@@ -85,7 +85,7 @@ int main ()
 
         // The following copies an integer (token) into the c-string that is the output buffer
 		sprintf(out_buffer, "%d", token_number);
-        assert(qd_client != null);
+        assert(qd_client != NULL);
 
         // Send the message to the client's mailbox/mailqueue
 		if (mq_send (qd_client, out_buffer, strlen (out_buffer), 0) == -1) {
