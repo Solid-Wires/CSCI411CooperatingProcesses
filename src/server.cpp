782b9b7bcec7c023650b0394f8b3fa6cba8d322b
Solid-Wires <solid.wires@gmail.com>
2024-02-12 19:42:51 -0500

    I had to remove the magic :(

--- src/server.cpp
+++ src/server.cpp
@@ -51,7 +51,4 @@ int main() {
     
     // Shutdown the server.
     shutdown_server_mq(0);
-}//http(set) code 1001111010         
-Debrief(qd input 69) "do it this way" mq-request ping //<Okay> /forsensik
-Forgein (request? client code 420 input --===ferror)
-//program 11011010^proset to max "okay"
+}
